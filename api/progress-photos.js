182404729ab35163c7963f3af78b4a9b081c4f49
dualperformance-ka <dualperformanceka@gmail.com>
2026-06-23 15:00:58 +0930

    Update progress-photos.js

--- api/progress-photos.js
+++ api/progress-photos.js
@@ -39,7 +39,7 @@ function inferPhotoType(publicId) {
     .join(' ') || 'Progress Photo';
 }
 
-module.exports = async function handler(req, res) {
+export default async function handler(req, res) {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET, OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
@@ -101,4 +101,4 @@ module.exports = async function handler(req, res) {
     console.error('[progress-photos]', e.message);
     res.status(500).json({ error: e.message });
   }
-};
+}
