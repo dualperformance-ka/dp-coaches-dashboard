48a758faac746be35ca068b68030ddc3b846bdda
dualperformance-ka <dualperformanceka@gmail.com>
2026-07-18 16:16:54 +0930

    Align mobile application cards with themes

--- public/sw.js
+++ public/sw.js
@@ -12,7 +12,7 @@
       would risk showing stale or cross-coach data offline)
    ============================================================ */
 
-const VERSION = 'dp-coaches-v14';
+const VERSION = 'dp-coaches-v15';
 const SHELL_CACHE = `${VERSION}-shell`;
 const STATIC_CACHE = `${VERSION}-static`;
 const FONT_CACHE = `${VERSION}-fonts`;
