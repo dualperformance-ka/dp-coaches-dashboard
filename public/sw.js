19e38f75e6d377d0aba5866859e1b5dbeb11979b
dualperformance-ka <dualperformanceka@gmail.com>
2026-07-30 10:52:54 +0930

    Add files via upload

--- public/sw.js
+++ public/sw.js
@@ -12,7 +12,7 @@
       would risk showing stale or cross-coach data offline)
    ============================================================ */
 
-const VERSION = 'dp-coaches-v18-mobile-data-parity';
+const VERSION = 'dp-coaches-v19-female-split-labels';
 const SHELL_CACHE = `${VERSION}-shell`;
 const STATIC_CACHE = `${VERSION}-static`;
 const FONT_CACHE = `${VERSION}-fonts`;
